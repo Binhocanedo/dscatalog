c7b4a9f505b8a23cb640e40a5aa315caed544a1f
FÁBIO CANEDO LEÃO <236462@facens.br>
2024-09-11 09:11:51 -0300

    CategoryResource

---
+++ backend/dscatalog/src/main/java/com/fabio/dscatalog/resources/CategoryResource.java
@@ -0,0 +1,25 @@
+package com.fabio.dscatalog.resources;
+
+import com.fabio.dscatalog.entities.Category;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll(){
+        List<Category> list = new ArrayList<>();
+        list.add(new Category(1L, "Books"));
+        list.add(new Category(2L, "Tech"));
+        list.add(new Category(3L, "SmartPhones"));
+
+        return ResponseEntity.ok().body(list);
+    }
+}

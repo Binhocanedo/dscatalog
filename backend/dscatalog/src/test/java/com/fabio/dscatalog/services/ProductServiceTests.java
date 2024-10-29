a79c82b4118468c835713f4a8a7f6faa8998ffdf
FÁBIO CANEDO LEÃO <236462@facens.br>
2024-10-29 06:24:27 -0300

    Teste findById, findAll, Pageable

--- backend/dscatalog/src/test/java/com/fabio/dscatalog/services/ProductServiceTests.java
+++ backend/dscatalog/src/test/java/com/fabio/dscatalog/services/ProductServiceTests.java
@@ -1,40 +1,82 @@
 package com.fabio.dscatalog.services;
 
+import br.com.six2six.fixturefactory.Fixture;
+import com.fabio.dscatalog.entities.Product;
+import com.fabio.dscatalog.exceptions.ApiException;
 import com.fabio.dscatalog.exceptions.ResourceNotFoundException;
 import com.fabio.dscatalog.repositories.ProductRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.mockito.Mockito.when;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
 
     @InjectMocks
     private ProductService service;
-
     @Mock
     private ProductRepository repository;
 
     Long existingId = 1L;
+    Long dependentId = 3L;
     Long nonExistingId = 1000L;
-
+    private PageImpl<Product> page;
+    private Product product;
 
     @BeforeEach
-    void setUp() throws Exception{
+    void setUp() throws Exception {
+
+        product = Fixture.from(Product.class).gimme("basic");
+        page = new PageImpl<>(List.of(product));
+
+        doNothing().when(repository).deleteById(existingId);
+        doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
+
+
+        when(repository.findAll((Pageable) ArgumentMatchers.any())).thenReturn(page);
+        when(repository.save(ArgumentMatchers.any())).thenReturn(product);
+
+        when(repository.findById(existingId)).thenReturn(Optional.of(product));
+        when(repository.findById(nonExistingId)).thenReturn(Optional.empty());
+
         when(repository.existsById(existingId)).thenReturn(true);
         when(repository.existsById(nonExistingId)).thenReturn(false);
+        when(repository.existsById(dependentId)).thenReturn(true);
+    }
+
+    @Test
+    public void deleteShouldDoNothingWhenIdExists() {
+        Assertions.assertDoesNotThrow(() ->{
+            service.delete(existingId);
+        });
+        verify(repository, times(1)).deleteById(existingId);
     }
 
     @Test
     public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist(){
         Assertions.assertThrows(ResourceNotFoundException.class, () -> {
             service.delete(nonExistingId);
+        });
+    }
+
+    @Test
+    public void deleteShouldDataBaseExceptionWhenDependentId() {
+        Assertions.assertThrows(ApiException.class, () -> {
+            service.delete(dependentId);
         });
     }
 }
